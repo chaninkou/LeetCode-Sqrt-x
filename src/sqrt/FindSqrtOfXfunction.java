6b49ef884a3cab309f2d0e77118136d179edafde
chaninkou <michaelkoulove@yahoo.com>
2020-02-10 21:46:12 -0800

    ok review on getting the whole number from sqrt without using built in.

--- src/sqrt/FindSqrtOfXfunction.java
+++ src/sqrt/FindSqrtOfXfunction.java
@@ -2,37 +2,42 @@ package sqrt;
 
 public class FindSqrtOfXfunction {
 	// Binary search way
-	// public int mySqrt(int x) {
-	// if(x == 0){
-	// return 0;
-	// }
+	public int mySqrt2(int x) {
+		if (x == 0) {
+			return 0;
+		}
 
-	// int left = 1;
-	// int right = x;
+		int left = 1;
+		int right = x;
 
-	// while(true){
-	// // To prevent overflow, use below instead of left + right /2
-	// int mid = left + ((right - left)/2);
+		while (true) {
 
-	// if(mid > x / mid){
-	// right = mid - 1;
-	// } else {
-	// if(mid + 1 > x / (mid + 1)){
-	// return mid;
-	// }
-	// left = mid + 1;
-	// }
-	// }
-	// }
+			// To prevent overflow, use below instead of left + right /2
+			int mid = left + ((right - left) / 2);
+
+			if (mid > x / mid) {
+				right = mid - 1;
+			} else {
+				if (mid + 1 > x / (mid + 1)) {
+					return mid;
+				}
+				
+				left = mid + 1;
+			}
+		}
+	}
 
 	// Integer Newton
 	public int mySqrt(int x) {
-		long r = x;
-
-		while (r * r > x) {
-			r = (r + x / r) / 2;
+		// Using long in case it will overflow when we check answer * answer > x
+		long answer = x;
+		
+		while(answer * answer > x){
+			
+			// Keep dividing by 2
+			answer = (answer + answer/x)/2;
 		}
-
-		return (int) r;
+		
+		return (int) answer;
 	}
 }
